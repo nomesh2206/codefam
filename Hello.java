2554603c038e47ce20193fcfe4dda76320ae1352
nomesh2206 <nomeshverma@gmail.com>
2023-05-18 11:15:24 +0530

    change NEW

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package mypackage;
+class Hello{
+    public static void main(String []args)
+    {
+        System.out.println("Hello World");
+		System.out.println("hello from another computer");
+        
+        System.out.println("knowit");
+    }
+}
